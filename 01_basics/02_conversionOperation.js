497957757d3227929edc3cb38d79a60c0fb8f2a1
ayushi03shukla-spec <ayushi03shukla@gmail.com>
2025-09-18 09:27:55 +0000

    store config

---
+++ 01_basics/02_conversionOperation.js
@@ -0,0 +1,8 @@
+ // "33"=33
+ //"33abc" =>NAN
+ //true => 1; false =>0
+ let isLoggedIn = "hitesh"
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+ // 1 => true; ,0=>false
+ //"hitesh" => true
---
+++ 01_basics/text.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) //we are using node js not browser
+ 
+ console.log("hitesh")
+ let name = "hitesh"
+ let age = 18
+ let isLoggedIn = false
+ // number =>2 to power 53
+ //big int
+ //string = " "
+ // boolean => true/false
+// null => standalone value
+// undefined => abhi value assign nhi hui h
+// symbol => unique
+//object
+console.log(typeof age)
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+ 
