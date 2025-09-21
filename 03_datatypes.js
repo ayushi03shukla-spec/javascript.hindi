475ba7b25506cac16f79ab8dbc96eee01d61bfdd
ayushi03shukla-spec <ayushi03shukla@gmail.com>
2025-09-21 12:46:28 +0000

    config done

---
+++ 03_datatypes.js
@@ -0,0 +1,21 @@
+ // primitive
+ // 7 types : String, Number, null, boolean, undefined, symbol, Big Int
+
+
+
+ // Reference(Non primitive)
+ // Arrays, Objects, Functions
+ //const heros = ["shaktiman","naagraj", "doga"]; // Array
+ let myObj =
+  {
+ name: "hitesh",
+age : 22,
+}// object
+const myFunction = function(){
+    //console.log("Hello world");
+
+}// functions
+console.log(typeof bigNumber);// undefined
+console.log(typeof myFunction);// function
+
+
